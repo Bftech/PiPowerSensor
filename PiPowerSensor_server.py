8e65a1aaeab1c5bdb2810654a291e2ca8261fdba
Bftech <corentin.monmasson@gmail.com>
2017-05-27 01:11:19 +0200

    controller start

--- PiPowerSensor_server.py
+++ PiPowerSensor_server.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/getPulses")
-def getPulses():
+@app.route("/getPulses/<periode>")
+def getPulses(periode):
+    if periode == "day":
+        return "day"
+    else if periode == "hour":
+        return "hour"
+    else if periode == "month":
+        return "month"
+        
     return "none";
 
 if __name__ == "__main__":
