e45860227fd10bda72d2f1db25ad92fa3330a4f2
Bftech <corentin.monmasson@gmail.com>
2017-05-28 11:17:49 +0200

    power

--- static/js/script.js
+++ static/js/script.js
@@ -15,15 +15,28 @@ $("input[name=refreshRate]").change(function() {
 });
 
 function refreshStats() {
+
+  var instantPower = getInstantPower();
+  console.log(instantPower + "kW");
+
+  $("#data1").text(i);
+  i++;
+  refreshId = setTimeout(refreshStats, speed * 1000);
+}
+
+function getInstantPower() {
   var powerData;
   $.get('http://'+ ip +':8086/query?db='+ db +'&epoch=ms&q=SELECT * FROM pulses WHERE location=\''+ loc +'\' ORDER BY time DESC LIMIT 2', function(data) {
     console.log(data);
     powerData = data;
   });
+  var time1 = powerData['results'][0]['series'][0]['values'][0][0]
+  var time2 = powerData['results'][0]['series'][0]['values'][1][0]
 
-  $("#data1").text(i);
-  i++;
-  refreshId = setTimeout(refreshStats, speed * 1000);
+  var intervalMS = time1 - time2;
+  var intervalSEC = intervalMS / 1000;
+
+  return 3600 / (intervalSEC * 1000);
 }
 
 function updateRefreshRate() {
