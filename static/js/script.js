28716c5abd950b554ac59a2669086f6dbef2ef58
Bftech <corentin.monmasson@gmail.com>
2017-05-28 11:08:29 +0200

    u

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,7 @@ $("input[name=refreshRate]").change(function() {
 
 function refreshStats() {
   var powerData;
-  $.get('http://'+ ip +':8086/query?db='+ db +'&epoch=ms&q=SELECT LAST(*) FROM pulses WHERE location=\''+ loc +'\' LIMIT 2', function(data) {
+  $.get('http://'+ ip +':8086/query?db='+ db +'&epoch=ms&q=SELECT * FROM pulses WHERE location=\''+ loc +'\' ORDER BY time DESC LIMIT 2', function(data) {
     console.log(data);
     powerData = data;
   });
